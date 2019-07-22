81061a31d7bb59823d779b365fc2b23682b44321
IamVeryDiao <lmx244620@gmail.com>
2019-07-23 01:13:51 +1200

    new version

---
+++ src/load/resume.js
@@ -0,0 +1,35 @@
+export const resume = `# Career Objective
+
+Junior Web Developer
+
+# Availability
+
+* HTML5
+* CSS3
+* JavaScript (ES5/ES6)
+* jQuery
+* BootStrap
+* React & Redux
+* Node.js
+
+# Professional Attributes
+
+* Strong Knowledge Background
+* Methodical
+* Solution Focused
+* Teamwork First
+* Goal Orientated
+* Work Under Pressure
+* Confident
+
+# Work Experience
+
+* THE UNIVERSITY OF AUCKLAND - JUNIOR RPA DEVELOPER (Intern)
+
+# Demo Online
+
+* [CSS_staticLayout](https://iamverydiao.github.io/CSS_fixedLayout/tryweb.html)
+* [React&Redux_shoppingCart](https://iamverydiao.github.io/REACT_shoppingCart/demo.html)
+* [React_palette](https://iamverydiao.github.io/REACT_palette/demo.html)
+* [React_counter](https://iamverydiao.github.io/REACT_counter/demo.html)
+`
---
+++ src/load/resume.md
@@ -0,0 +1,37 @@
+# Career Objective
+
+Junior Web Developer
+
+# Availability
+
+* HTML5
+* CSS3
+* JavaScript (ES5/ES6)
+* jQuery
+* BootStrap
+* React & Redux
+* Node.js
+
+# Professional Attributes
+
+* Strong Knowledge Background
+* Methodical
+* Solution Focused
+* Teamwork First
+* Goal Orientated
+* Work Under Pressure
+* Confident
+
+# Work Experience
+
+* THE UNIVERSITY OF AUCKLAND - JUNIOR RPA DEVELOPER (Intern)
+
+# Demo Online
+
+* [CSS_staticLayout](https://iamverydiao.github.io/fixed_page_layout/tryweb.html)
+* [React&Redux_shoppingCart](https://iamverydiao.github.io/shopping_cart/demo.html)
+* [React_palette](https://iamverydiao.github.io/palette/demo.html)
+* [React_counter](https://iamverydiao.github.io/counter/demo.html)
+
+
+
