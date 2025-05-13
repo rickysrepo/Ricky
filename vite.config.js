850f5361b6a3ffbbf7b98269974bc2b48352d665
Ricky <lmx244620@gmail.com>
2025-05-13 16:40:27 +1200

    Change Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   root: 'src',
   publicDir: 'public',
   build: {
-    outDir: '../docs',
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
